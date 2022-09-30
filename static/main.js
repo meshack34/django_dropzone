651df959249649bb0ce36f5f0cd641b1a92b7990
meshack34 <meshackkimutai34@gail.com>
2022-09-30 12:41:14 +0300

    update

--- static/main.js
+++ static/main.js
@@ -1,15 +1,9 @@
 
-// Dropzone.autoDiscover=false;
-// const myDropzone= new Dropzone('#my-dropzone',{
-//     url:'upload/',
-//     maxFiles:5,
-//     maxFilesize:2,
-//     acceptedFiles:'.jpg',
-// })
+
 Dropzone.autoDiscover=true;
 var myDropzone = new Dropzone("#designdocumentexcel",
 {
-    acceptedFiles : ".xls,.xlsx,jpg,.csv,.docs",
+    acceptedFiles : ",jpg,.csv,.docs,.pdf",
     init: function(){
         this.on("error", function(file, errorMessage) {
             alert("error : " + errorMessage );
